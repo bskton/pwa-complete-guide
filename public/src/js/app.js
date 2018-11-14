410634eaa3ab680fdb4b7a93f23c5c0b98e05c16
Ilya Levin <bskton@gmail.com>
2018-11-14 21:28:08 +1000

    Fetch logging was removed from service worker
    Also
     - Function for creating card was added.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,9 +5,14 @@ if (!window.Promise) {
 }
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("Service worker registered.");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Service worker registered!");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 window.addEventListener("beforeinstallprompt", event => {
